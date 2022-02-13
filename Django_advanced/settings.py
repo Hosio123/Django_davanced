c59ff0e4ca6c0441f473f60d7c145f1e0655c8d4
Hosio123 <southpaw@kakao.com>
2022-02-13 18:10:04 +0900

    make tabom model

--- Django_advanced/settings.py
+++ Django_advanced/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS: List[str] = []
 # Application definition
 
 INSTALLED_APPS = [
+    "tabom.apps.TabomConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -130,3 +131,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from Django_advanced.local_settings import *
+except ImportError:
+    pass
